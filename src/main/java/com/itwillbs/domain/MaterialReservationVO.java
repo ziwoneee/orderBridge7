79b924c343b20f792a4b0ebd1a4dbc322f79bd55
ziwoneee <ziwonee24@gmail.com>
2025-08-11 11:08:01 +0900

    자재 예약 VO 생성

---
+++ src/main/java/com/itwillbs/domain/MaterialReservationVO.java
@@ -0,0 +1,20 @@
+package com.itwillbs.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+/**
+ * [자재 예약 VO]
+ * - material_reservation 테이블과 매핑
+ * - 예약은 "작업지시서 + 자재" 단위로 묶임
+ */
+@Data
+public class MaterialReservationVO {
+	
+	private String workOrderNo;			// 작업지시서 번호
+	private String materialId;			// 자재 ID
+	private int reservedQty;			// 예약 수량 (해당 작업지시서가 확보해둔 수량)
+	private Date updatedAt;				// 수정 시간
+
+}
